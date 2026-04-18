746fa8d5372094e4545821b4aa9bb3f2dddba473
Jayson Palacio <jaysonpalacio1@outlook.com>
2026-04-18 00:49:49 -0400

    fix: remove duplicate const declaration

--- src/components/CharacterDisplay.js
+++ src/components/CharacterDisplay.js
@@ -1526,7 +1526,6 @@ const Guardian = () => (
 );
 
 const Jesus = () => (
-const Jesus = () => (
   <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
     <defs>
       <radialGradient id="js-skin" cx="40%" cy="35%"><stop offset="0%" stopColor="#FDDBA8"/><stop offset="100%" stopColor="#C8925A"/></radialGradient>
